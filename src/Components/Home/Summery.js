5df28fcdc904ec2ffd5309656bf3d993e2bec651
Md Asaduzaman <asadasad9282@gmail.com>
2022-05-24 03:21:35 +0600

    simple buisness summry

--- src/Components/Home/Summery.js
+++ src/Components/Home/Summery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CountUp from 'react-countup';
 
 const Summery = () => {
 
@@ -34,8 +35,39 @@ const Summery = () => {
 
     return (
         <div>
-            <h2 className='mt-5 mb-5 text-3xl bg-red-900 p-20 text-white'>THis is Summery page</h2>
+            <h2 className=' mt-14 text-3xl bg-primary p-4 text-white'>THis is Summery page</h2>
+            <div class="stats stats-vertical lg:stats-horizontal shadow">
 
+                <div class="stat">
+                    <div class="stat-title">Downloads</div>
+                    <div class="stat-value">31K</div>
+                    <div class="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div class="stat">
+                    <div class="stat-title">New Users</div>
+                    <div class="stat-value">4,200</div>
+                    <div class="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div class="stat">
+                    <div class="stat-title">New Registers</div>
+                    <div class="stat-value"><CountUp end={1155} /> </div>
+                    <div class="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+                <div class="stat">
+                    <div class="stat-title">New Registers</div>
+                    <div class="stat-value">900</div>
+                    <div class="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+                <div class="stat">
+                    <div class="stat-title">New Registers</div>
+                    <div class="stat-value">1,200</div>
+                    <div class="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+            </div>
         </div>
     );
 };
